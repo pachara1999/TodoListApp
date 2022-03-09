269d1121069aa40b3d878e2f5d87cc31d2d32d15
bright <bite.c67@gmail.com>
2022-03-09 15:57:01 +0700

    Add Method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,41 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using todolist.Models;
+using TodoListApp.Models;
 
 namespace todolist.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
     {
-        return View();
+        List<Todolist> lists = _db.todolist.ToList();
+        return View(lists);
+    }
+
+    [HttpPost]
+    public IActionResult AddList(Todolist todolist)
+    {
+        try
+        {
+            _db.todolist.Add(todolist);
+            _db.SaveChanges();
+            return Redirect("~/Home/Index");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return Redirect("~/Home/Index");
     }
 
     public IActionResult Privacy()
